3950734e48e4671a05edb63edabbbd05541bea63
C.T. Bell <charlestbell@gmail.com>
2021-04-04 12:32:20 -0400

    Turned Navigation into a component

--- client/src/App.js
+++ client/src/App.js
@@ -1,69 +1,19 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-// import Nav from "./components/Nav";
+import Nav from "./components/Nav";
 import Home from "./components/Home";
 import Portfolio from "./components/Portfolio";
 import Resume from "./components/Resume";
 
 function App() {
-  // mainPage = document.querySelector("#mainPage");
-  // portfolioPage = document.querySelector("#portfolioPage");
-  // resumePage = document.querySelector("#resumePage");
-  // navigation = document.querySelector("#navigation");
-
-  // navigation.addEventListener("click", (event) => {
-  //   switch (event.target.id) {
-  //     case "resumeBtn":
-  //       console.log("resume button");
-  //       mainPage.setAttribute("class", "d-none");
-  //       portfolioPage.setAttribute("class", "d-none");
-  //       resumePage.classList.remove("d-none");
-  //       break;
-  //     case "portfolioBtn":
-  //       console.log("portfolio button");
-  //       mainPage.setAttribute("class", "d-none");
-  //       resumePage.setAttribute("class", "d-none");
-  //       portfolioPage.classList.remove("d-none");
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // });
-
   return (
     <Router>
       <div>
         <main className="container mb-3">
           {/* Navigation */}
           <div className="row" id="navigation">
-            <nav className="col-md-3 d-flex justify-content-center mt-2 mt-md-5">
-              <div className="col text-center ">
-                <h3 className=" hoverBtn">
-                  <Link className=" ml-2" to="/">
-                    C.T. Bell
-                  </Link>
-                </h3>
-                <Link to="/portfolio">
-                  <button
-                    type="button"
-                    id="portfolioBtn"
-                    className=" btn btn-lg mt-1 ml-2 text-light mb-2 shadow hoverBtn"
-                  >
-                    Portfolio
-                  </button>
-                </Link>
-                <Link to="/resume">
-                  <button
-                    type="button"
-                    id="resumeBtn"
-                    className=" btn btn-lg mt-1 ml-2 text-light mb-2 shadow hoverBtn"
-                  >
-                    Resume
-                  </button>
-                </Link>
-              </div>
-            </nav>
+            <Nav />
             <div className="col-md-9 mt-4">
               <Switch>
                 <Route exact path="/">
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,29 +1,33 @@
 import React from "react";
-import { BrowserRouter as Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
     <nav className="col-md-3 d-flex justify-content-center mt-2 mt-md-5">
       <div className="col text-center ">
         <h3 className=" hoverBtn">
-          <a className=" ml-2" href="/">
+          <Link className=" ml-2" to="/">
             C.T. Bell
-          </a>
+          </Link>
         </h3>
-        <button
-          type="button"
-          id="portfolioBtn"
-          className=" btn btn-lg mt-1 ml-2 text-light mb-2 shadow hoverBtn"
-        >
-          <Link to="/portfolio"> Portfolio</Link>
-        </button>
-        <button
-          type="button"
-          id="resumeBtn"
-          className=" btn btn-lg mt-1 ml-2 text-light mb-2 shadow hoverBtn"
-        >
-          Resume
-        </button>
+        <Link to="/portfolio">
+          <button
+            type="button"
+            id="portfolioBtn"
+            className=" btn btn-lg mt-1 ml-2 text-light mb-2 shadow hoverBtn"
+          >
+            Portfolio
+          </button>
+        </Link>
+        <Link to="/resume">
+          <button
+            type="button"
+            id="resumeBtn"
+            className=" btn btn-lg mt-1 ml-2 text-light mb-2 shadow hoverBtn"
+          >
+            Resume
+          </button>
+        </Link>
       </div>
     </nav>
   );
